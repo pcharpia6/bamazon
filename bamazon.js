be052828c64b833eb316b98280f70b7eb28ef5d1
Philip M. Charpia <46582563+pcharpia6@users.noreply.github.com>
2019-06-02 20:51:04 -0400

    stock check function working

--- bamazon.js
+++ bamazon.js
@@ -12,8 +12,70 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected");
-    begin();
+    beginBuy();
 });
+
+function beginBuy() {
+    connection.query(
+        "SELECT item_id, product_name, price FROM products",
+        function(err, res) {
+            if (err) throw err;
+            console.log("ITEMS FOR SALE:\n")
+            for (let i = 0; i < res.length; i++) {console.log(res[i])}
+            buyWhat();
+        }
+    )
+};
+
+function buyWhat() {
+    Inquirer.prompt([
+        {
+        type: "input",
+        message: "What is the ID of the item you would like to buy?\n",
+        name: "buy_id",
+        validate: function(buy_id) {return buy_id == parseInt(buy_id.trim())}
+        },
+        {
+        type: "input",
+        message: "How many would you like to buy?\n",
+        name: "buy_quant",
+        validate: function(buy_quant) {return  buy_quant == buy_quant.trim()}
+        }
+        ]).then((res) => {
+            console.log("buywhat response: " + res);
+            stockQuant(res);
+        })
+};
+
+function stockQuant(input) {
+    connection.query(
+        "SELECT stock_quantity FROM products WHERE ?",
+        {
+            item_id: input.buy_id
+        },
+        function(err, res) {
+            if (err) throw err;
+            let stock = res[0].stock_quantity;
+            if (parseInt(stock) >= parseInt(input.buy_quant)) {
+                pullStock(input);
+                console.log("Your purchase is successful.")
+            } else {
+                console.log(parseInt(parseInt(input.buy_quant)));
+                console.log("Not enough item in stock.\nPlease try again.");
+                beginBuy();
+            }
+        }
+    );
+};
+
+function pullStock(input) {
+    connection.query(
+        "UPDATE stock_quantity FROM products WHERE ?",
+        {
+            item_id: input.item_id
+        }
+    )
+}
 
 function begin() {
     Inquirer.prompt([
@@ -76,3 +138,7 @@ var sellItem = function() {
         )
     }
 };
+
+var buyItem = function() {
+
+}
